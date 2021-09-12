708f7439cf490041fa573112f6cba89be4720d75
evelientje90 <evelina.kupryte1990@gmail.com>
2021-09-12 10:59:49 +0200

    Update index.js
    Changed current temperature and city  in js

--- src/index.js
+++ src/index.js
@@ -122,3 +122,4 @@ form.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
+searchCity("Amsterdam");
